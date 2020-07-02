c5fb23eb8caab60537598db935f2859233f1f78a
Dmitry Fedorinov <fedorinov.dmitry@gmail.com>
2020-07-02 12:49:34 +0300

    Initial Commit

---
+++ SimpleMeet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMeet
+//
+//  Created by Дмитрий Федоринов on 02.07.2020.
+//  Copyright © 2020 Дмитрий Федоринов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleMeetTests/SimpleMeetTests.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleMeetTests.swift
+//  SimpleMeetTests
+//
+//  Created by Дмитрий Федоринов on 02.07.2020.
+//  Copyright © 2020 Дмитрий Федоринов. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleMeet
+
+class SimpleMeetTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
